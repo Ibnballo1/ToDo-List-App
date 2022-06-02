26f534ea302cf9d71c70f419454beee139fd17c0
Ibnballo1 <belloabdullateef035@gmail.com>
2022-06-02 16:17:30 +0100

    Update index js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
 import './style.css';
+
+const addedItems = 
+
+let todoTasks = [
+    {
+        description: 'wash the dishes',
+        index: 1,
+        completed: false,
+    },
+    {
+        description: 'Complete Todo List Project',
+        index: 2,
+        completed: true,
+    },
+    {
+        description: 'Code Daily',
+        index: 3,
+        completed: false
+    },
+];
+
+// Function to iterate over the task array
+const eachTask = todoTasks.forEach(()=> {
+    const div = document.createElement('div');
+    console.log(div)
+});
+
+eachTask();
