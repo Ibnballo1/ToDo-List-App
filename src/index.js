d605f4c1a44ce296a8df7d786d7d6fc1bbfd1e82
Ibnballo1 <belloabdullateef035@gmail.com>
2022-06-02 23:14:06 +0100

    Fix code to populate according to index

--- src/index.js
+++ src/index.js
@@ -11,28 +11,39 @@ const todoTasks = [
   },
   {
     description: 'Complete Todo List Project',
-    index: 2,
+    index: 3,
     completed: false,
   },
   {
     description: 'Code Daily',
-    index: 3,
+    index: 2,
+    completed: false,
+  },
+  {
+    description: 'Exercise',
+    index: 5,
+    completed: false,
+  },
+  {
+    description: 'Work',
+    index: 4,
     completed: false,
   },
 ];
 
 // Function to iterate over the task array
 const populateTask = todoTasks.forEach((todos) => {
-  const { description, completed, index } = todos;
+  const { index } = todos;
   const eachTask = `
     <div id="each-task" class="item-box-${counter + 1} each-task">
         <div class="inputs">
-            <input type="checkbox" id="mark" name="mark" class='check${index} ${completed} mark' value="Check">
-            <label for="mark"> ${description} </label>
+            <input type="checkbox" id="mark" name="mark" class='check${todoTasks[index-1].index} ${todoTasks[index-1].completed} mark' value="Check">
+            <label for="mark"> ${todoTasks[index-1].description} </label>
         </div>
         <button type="button" id="del"><i class="fas fa-trash-alt"></i></button>
     </div>
 `;
+
   addedItems.insertAdjacentHTML('beforeend', eachTask);
   counter += 1;
 });
