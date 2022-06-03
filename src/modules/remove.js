1b7feadd7fad80dc6c5c41f6f77af15561a19255
Ibnballo1 <belloabdullateef035@gmail.com>
2022-06-03 15:24:49 +0100

    Fix remove function

--- src/modules/remove.js
+++ src/modules/remove.js
@@ -5,12 +5,9 @@ function removeTask() {
     // Remove Task After it has been added
     const rmvEachTask = document.getElementsByClassName('rmvBtn');
     for (let i = 0; i < todoTasks.length; i++) {
-            todoTasks[i].addEventListener('click', () => {
-            //    console.log('I am clicked')
-                todoTasks[i].remove();
-                //console.log(todoTasks[i]);
-            });
-            //console.log('Am working')
+        todoTasks[i].querySelector('.rmvBtn').addEventListener('click', () => {
+            todoTasks[i].remove();
+        });
     }
 };
 
