7f74a8635c83bc320cb103c49417dfce08ad0659
Ibnballo1 <belloabdullateef035@gmail.com>
2022-06-04 10:49:02 +0100

    Add list elements

--- src/modules/forTask.js
+++ src/modules/forTask.js
@@ -22,8 +22,31 @@ export const addedItemDiv = document.getElementById('added-items');*/
             isCompleted: isCompleted ?? false,
             index: index ?? this.addedItems.length,
             id: this.taskIndex,
-        }
-    }
+        };
+        newTask.index += 1;
+        this.addedItems.push(newTask);
+        this.taskIndex += 1;
+        this.setData();
+        return newTask;
+    };
+    addLiTag = (task) => {
+        const ul = document.getElementById('addedItems');
+        const li = document.createElement('li');
+        const checkbox = document.createElement('INPUT');
+        const input = document.createElement('INPUT');
+        const button = document.createElement('button');
+        button.innerHTML = 'remove';
+        li.setAttribute('id', `li${task.id}`);
+        checkbox.setAttribute('id', `checkbox${task.id}`);
+        input.setAttribute('id', `input${task.id}`);
+        button.setAttribute('id', `button${task.id}`);
+        checkbox.setAttribute('type', 'checkbox');
+        input.setAttribute('type', 'text');
+        checkbox.checked = task.isCompleted;
+        input.value = task.description;
+        li.append(checkbox, input, button);
+        ul.append(li);
+    };
 };
 
 form.addEventListener('submit', addTasks);
