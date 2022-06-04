bc4cc4f42699356297ddc9d57063ce974c02fa98
Ibnballo1 <belloabdullateef035@gmail.com>
2022-06-04 11:08:51 +0100

    Import class MyTasks to index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
-import inputs from './modules/forTask';
+/*import inputs from './modules/forTask';*/
+import MyTasks from './modules/forTask';
 import './style.css';
-import getDatas from './modules/local-storage';
+/*import getDatas from './modules/local-storage';
 import addTasks from './modules/addtasks';
 import storeData from './modules/local-storage';
 export let counter = 0;
@@ -8,6 +9,17 @@ export const todoTasks = [];
 
 const addedItems = document.getElementById('added-items');
 
-getDatas();
+getDatas();*/
+
+const form = document.getElementById('formAddItems');
+const addDescription = document.getElementById('toAdd');
+const listTitle = document.getElementById("listTitle");
+let dataCount = 0;
+const getStorage = localStorage.getItem('dataCount');
+if (getStorage) {
+    dataCount = getStorage;
+}
+
+const eachList = new List(dataCount);
 
 
