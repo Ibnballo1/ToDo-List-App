1452d250bc457f84feb485fdea1d6b2b453e5552
Ibnballo1 <belloabdullateef035@gmail.com>
2022-06-04 10:30:57 +0100

    Edit class in fortasks

--- src/modules/forTask.js
+++ src/modules/forTask.js
@@ -1,21 +1,19 @@
-import { counter } from "..";
+/*import { counter } from "..";
 import { todoTasks } from "..";
 import removeTask from "./remove";
 import getDatas from "./local-storage";
 import addTasks from "./addtasks";
-
+*/
 // Variables for all elemnts
-const form = document.getElementById('form-add-items');
-export const inputs = document.getElementById('to-add');
-const inputBtn = document.getElementById('add');
-export const addedItemDiv = document.getElementById('added-items');
+
+/*const inputBtn = document.getElementById('add');
+export const addedItemDiv = document.getElementById('added-items');*/
 
 // Create object for task to be added to array
- export class MyTasks{
-    constructor(description = "", index, completed = false) {
-        this.description = description;
-        this.index = index;
-        this.completed = completed;
+ class MyTasks{
+    constructor(taskIndex) {
+        this.addedItems = [];
+        this.taskIndex = Number(taskIndex);
     }
 };
 
